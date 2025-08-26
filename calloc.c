68d7a0e49cdc8e4d7422e558d77350b8412253b8
pratyushshekharr-source <pratyushshekharr@gmail.com>
2025-08-26 20:14:28 +0530

    Daynamic Memory Allocation

---
+++ calloc.c
@@ -0,0 +1,12 @@
+// # include <stdio.h>
+// # include <stdlib.h>
+// int main(){
+//   int *ptr,n;
+//   printf("Enter the n value : ");
+//   scanf("%d",&n);
+//   ptr  =(int*) calloc(n , sizeof(int));
+//   for (int i = 0; i <= n; i++){
+//     printf("%d\n",ptr[i]);
+//   }
+//   return 0;
+// }
---
+++ malloc.c
@@ -0,0 +1,15 @@
+// # include <stdio.h>
+// # include <stdlib.h>
+// int main(){
+//   int *ptr;
+//   ptr  =(int*)malloc(5 * sizeof(int));
+//   ptr[0] = 2;
+//   ptr[1] = 4;
+//   ptr[2] = 6;
+//   ptr[3] = 8;
+//   ptr[4] = 10;
+//   for (int i = 0; i < 5; i++){
+//     printf("%d\n",ptr[i]);
+//   }
+//   return 0;
+// }
